35b7fe3165d8c94521851f4645debe1102f77895
Bard <admin@cooluc.com>
2024-02-26 13:09:38 +0800

    golang: update to 1.22
    Signed-off-by: Bard <admin@cooluc.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -35,7 +35,7 @@ cat feeds.conf
 rm -rf feeds/packages/net/{v2ray-core,v2ray-geodata,v2raya,xray-core}
 
 rm -rf feeds/packages/lang/golang
-git clone https://github.com/sbwml/packages_lang_golang -b 21.x feeds/packages/lang/golang > /dev/null
+git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
 
 make defconfig > /dev/null
 
