eb24f665da0cb6aaa12d2ac5341b114dc7c15028
Bard <admin@cooluc.com>
2024-08-14 13:39:04 +0800

    go1.23
    Signed-off-by: Bard <admin@cooluc.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -32,10 +32,10 @@ cat feeds.conf
 
 ./scripts/feeds update -a > /dev/null
 
-rm -rf feeds/packages/net/{v2ray-core,v2ray-geodata,v2raya,xray-core}
+rm -rf feeds/packages/net/{v2ray-core,v2ray-geodata,v2raya,xray-core,sing-box}
 
 rm -rf feeds/packages/lang/golang
-git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
 
 make defconfig > /dev/null
 
